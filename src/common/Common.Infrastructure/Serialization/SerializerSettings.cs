51b7add291398a2b0a722d25aa6a2eeab06e837d
saifd <saifd@SAIFPC>
2024-12-07 10:49:34 +0200

    rfdg

--- src/common/Common.Infrastructure/Serialization/SerializerSettings.cs
+++ src/common/Common.Infrastructure/Serialization/SerializerSettings.cs
@@ -6,9 +6,7 @@ public static class SerializerSettings
 {
     public static readonly JsonSerializerSettings Instance = new()
     {
-#pragma warning disable CA2326 // Do not use TypeNameHandling values other than None
         TypeNameHandling = TypeNameHandling.All,
-#pragma warning restore CA2326 // Do not use TypeNameHandling values other than None
         MetadataPropertyHandling = MetadataPropertyHandling.Default
     };
 }
