61aec4f98b2481ff3aecd098311c21b955edc8c7
SaifU <Saif@SAIF-PC>
2024-12-09 15:35:26 +0200

    generic blazor cleanup

--- src/clients/BlazorMaster/Services/GenericService.cs
+++ src/clients/BlazorMaster/Services/GenericService.cs
@@ -2,41 +2,33 @@
 
 namespace BlazorMaster.Services;
 
-internal class GenericService<T>(HttpClient httpClient, string apiUrl) : IGenericService<T> where T : class
+internal sealed class GenericService<T>(HttpClient httpClient, string apiUrl) : IGenericService<T> where T : class
 {
     private readonly HttpClient _httpClient = httpClient;
     private readonly string _apiUrl = apiUrl;
 
-    public async Task<T> CreateAsync(T entity)
+    public async Task<T?> CreateAsync(T? entity)
     {
         HttpResponseMessage response = await _httpClient.PostAsJsonAsync(_apiUrl, entity);
-        if (response.IsSuccessStatusCode)
-        {
-            return await response.Content.ReadFromJsonAsync<T>();
-        }
-        return null;
+        return response.IsSuccessStatusCode ? await response.Content.ReadFromJsonAsync<T>() : null;
     }
 
-    public async Task<List<T>> GetAllAsync()
+    public async Task<List<T?>> GetAllAsync()
     {
         List<T>? response = await _httpClient.GetFromJsonAsync<List<T>>(_apiUrl);
-        return response ?? [];
+        return response!;
     }
 
-    public async Task<T> GetByIdAsync(int id)
+    public async Task<T?> GetByIdAsync(int? id)
     {
         T? response = await _httpClient.GetFromJsonAsync<T>($"{_apiUrl}/{id}");
         return response;
     }
 
-    public async Task<T> UpdateAsync(int id, T entity)
+    public async Task<T?> UpdateAsync(int? id, T? entity)
     {
         HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"{_apiUrl}/{id}", entity);
-        if (response.IsSuccessStatusCode)
-        {
-            return await response.Content.ReadFromJsonAsync<T>();
-        }
-        return null;
+        return response.IsSuccessStatusCode ? await response.Content.ReadFromJsonAsync<T>() : null;
     }
 
     public async Task<bool> DeleteAsync(int id)
@@ -45,9 +37,14 @@ internal class GenericService<T>(HttpClient httpClient, string apiUrl) : IGeneri
         Uri requestUri = new($"{_apiUrl}/{id}");
 
         // Send DELETE request using the Uri
-        var response = await _httpClient.DeleteAsync(requestUri);
+        HttpResponseMessage response = await _httpClient.DeleteAsync(requestUri);
 
         // Return true if the response is successful
         return response.IsSuccessStatusCode;
+    }
+
+    public Task<bool> DeleteAsync(int? id)
+    {
+        throw new NotImplementedException();
     }
 }
