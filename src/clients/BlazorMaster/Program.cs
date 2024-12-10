57c6f2d2bfc79c107368c8eec3c23475f5be5a61
SaifU <Saif@SAIF-PC>
2024-12-10 15:25:35 +0200

    final work v00

--- src/clients/BlazorMaster/Program.cs
+++ src/clients/BlazorMaster/Program.cs
@@ -7,13 +7,14 @@ using BlazorMaster;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
+builder.Services.AddMudServices();
 builder.Services.AddClientLibrary();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
